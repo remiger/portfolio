9f09dd1142a0897938198bfc257a76d32448c61d
Camille Semaan <csemaan@cmaisonneuve.qc.ca>
2022-02-09 12:48:42 -0500

    Raccourci clavier React

--- src/ListeProduits.jsx
+++ src/ListeProduits.jsx
@@ -7,12 +7,7 @@ export default function ListeProduits() {
         <section className="ListeProduits">
             <h2>Nos produits</h2>
             <div>
-                <article>
-                    <img src="" alt="" />
-                    <div className="titre">Titre du produit</div> 
-                    <div className="prix">13.95</div>
-                    <button>Ajouter au panier</button>
-                </article>
+                
                 <article>Produit 2</article>
                 <article>Produit 3</article>
                 <article>Produit 4</article>
