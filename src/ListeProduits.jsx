c3ef9a2331851a354222f3d3a2cd1bbad286a89a
Camille Semaan <csemaan@cmaisonneuve.qc.ca>
2022-02-09 13:09:06 -0500

    Composant Produit

--- src/ListeProduits.jsx
+++ src/ListeProduits.jsx
@@ -1,4 +1,5 @@
 import './ListeProduits.css';
+import Produit from './Produit';
 
 export default function ListeProduits() {
     let a = 5;
@@ -6,12 +7,13 @@ export default function ListeProduits() {
     return (
         <section className="ListeProduits">
             <h2>Nos produits</h2>
-            <div>
+            <div className="produits">
+                <Produit/>
+                <Produit/>
+                <Produit/>
+                <Produit/>
+                <Produit/>
                 
-                <article>Produit 2</article>
-                <article>Produit 3</article>
-                <article>Produit 4</article>
-                <article>Produit 5</article>
             </div>
         </section>
     );
--- src/Produit.jsx
+++ src/Produit.jsx
@@ -1,4 +1,12 @@
 import './Produit.css';
-export default function Produit() {
 
+export default function Produit() {
+    return (
+        <article className="Produit">
+            <img src="images-produits/prd0002.webp" alt="T-shirt sport GapFit recyclé" />
+            <div className="titre">T-shirt sport GapFit recyclé</div> 
+            <div className="prix">13.95</div>
+            <button>Ajouter au panier</button>
+        </article>
+    );
 }
