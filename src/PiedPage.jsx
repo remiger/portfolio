660e0a603aea5713c7e0222e386131777a49216b
Remi Gervais <e2095138@cmaisonneuve.qc.ca>
2022-02-21 20:19:14 -0500

    ajout cv

--- src/PiedPage.jsx
+++ src/PiedPage.jsx
@@ -1,9 +1,11 @@
 import './PiedPage.scss';
 import piedpage from './data/entetes.json';
+import Image from './Image';
 
 export default function PiedPage() {
     return (
         <footer id={piedpage.map(id => id.idfooter)} className="PiedPage">
+            <Image infoimg="Cliquez pour télécharger mon CV" nom="CV" lien="pdf/CV_Remi_Gervais.pdf"/>
             &copy;{new Date().getFullYear()} - Rémi Gervais
         </footer>
     );
