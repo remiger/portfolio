f7ea22952943e9549e1056a079de3457f6d3aa60
Remi Gervais <e2095138@cmaisonneuve.qc.ca>
2022-03-01 20:01:40 -0500

    fini version affichable avec tous liens qui fonctionnent

--- src/PiedPage.jsx
+++ src/PiedPage.jsx
@@ -1,13 +1,24 @@
 import './PiedPage.scss';
 import piedpage from './data/entetes.json';
-import Image from './Image';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function PiedPage() {
     return (
         <footer id={piedpage.map(id => id.idfooter)} className="PiedPage">
             <h1>Pour me contacter</h1>
-            <div><Image infoimg="Cliquez pour télécharger mon CV" nom="CV" lien="pdf/CV_Remi_Gervais.pdf"/></div>
-            <div>{new Date().getFullYear()} - Rémi Gervais</div>
+            <div className="PiedPage__CV">
+                <a href="https://ca.linkedin.com/in/r%C3%A9mi-gervais-24a6a9180" rel="noreferrer" target="_blank" >
+                    <LinkedInIcon sx={{ fontSize: 45 }} />
+                </a>
+                <a href="https://github.com/remiger" rel="noreferrer" target="_blank">
+                    <GitHubIcon sx={{ fontSize: 40 }} />
+                </a>
+                <a href="pdf/CV_Remi_Gervais.pdf" rel="noreferrer" target="_blank">
+                    <button>Télécharger mon CV</button>
+                </a>
+            </div>
+            <div>&copy;{new Date().getFullYear()} - Rémi Gervais</div>
         </footer>
     );
 }
